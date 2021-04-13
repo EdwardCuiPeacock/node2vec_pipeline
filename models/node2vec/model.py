6198e39e7d60084024efe71e1741ca949d30827c
EdwardCuiPeacock <Edward.Cui@nbcuni.com>
2021-04-13 11:18:48 -0400

    switch to numpy skipgram generation

--- models/node2vec/model.py
+++ models/node2vec/model.py
@@ -243,28 +243,28 @@ def _create_sampled_training_data(
     logging.info("Generate Skipgrams")
     for phase in ["train", "eval"]:
         cur_seed = (cur_seed + 1) if seed is not None else None
-        
-        saved_results, num_rows_saved = generate_skipgram_beam(
-            sample_metadata[phase]["random_walk_uri_list"],
-            vocabulary_size=num_nodes,
-            window_size=window_size,
-            negative_samples=negative_samples,
-            seed=cur_seed,
-            feature_names=[f"s{i}" for i in range(walk_length)],
-            save_path=os.path.join(storage_path, phase, "skipgrams"),
-            temp_dir=temp_dir,
-            beam_pipeline_args=beam_pipeline_args,
-        )
 
-        # saved_results, num_rows_saved = generate_skipgram_numpy(
+        # saved_results, num_rows_saved = generate_skipgram_beam(
         #     sample_metadata[phase]["random_walk_uri_list"],
-        #     vocab_size=num_nodes,
+        #     vocabulary_size=num_nodes,
         #     window_size=window_size,
         #     negative_samples=negative_samples,
         #     seed=cur_seed,
-        #     buffer_size=100,
-        #     save_path=os.path.join(storage_path, phase),
+        #     feature_names=[f"s{i}" for i in range(walk_length)],
+        #     save_path=os.path.join(storage_path, phase, "skipgrams"),
+        #     temp_dir=temp_dir,
+        #     beam_pipeline_args=beam_pipeline_args,
         # )
+
+        saved_results, num_rows_saved = generate_skipgram_numpy(
+            sample_metadata[phase]["random_walk_uri_list"],
+            vocab_size=num_nodes,
+            window_size=window_size,
+            negative_samples=negative_samples,
+            seed=cur_seed,
+            buffer_size=100,
+            save_path=os.path.join(storage_path, phase),
+        )
 
         sample_metadata[phase]["skipgram_uri_list"] = saved_results
         sample_metadata[phase]["data_size"] = num_rows_saved

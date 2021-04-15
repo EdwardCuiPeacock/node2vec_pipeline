81d0d7532107c937b954eee9cca229d1ed036aa4
EdwardCuiPeacock <Edward.Cui@nbcuni.com>
2021-04-15 16:38:16 -0400

    m

--- models/node2vec/model.py
+++ models/node2vec/model.py
@@ -377,7 +377,8 @@ def run_fn(fn_args):
         seed=model_config["seed"],
     )
     for kk, ii in enumerate(train_dataset):
-        print(kk, ii.shape)
+        print(kk)
+        print(ii)
 
     eval_batch_size = model_config.get("eval_batch_size") or train_batch_size
     eval_dataset = _input_fn(
